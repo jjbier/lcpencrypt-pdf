3b5c86028af327f85c231c659aa348dea6b6b1d0
Xabier Fernández <jjbier@gmail.com>
2018-12-28 23:10:11 +0100

    add encrypt ad decrypt library for pdf files

---
+++ encrypt/encrypt.go
@@ -0,0 +1,131 @@
+package encrypt
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"github.com/readium/readium-lcp-server/crypto"
+	"io/ioutil"
+	"os"
+)
+
+// EncryptedPdf Encrypted pdf
+type EncryptedPdf struct {
+	Path          string
+	EncryptionKey []byte
+	Size          int64
+	Checksum      string
+}
+
+// DecryptedPdf plain pdf
+type DecryptedPdf struct {
+	Path     string
+	Size     int64
+	Checksum string
+}
+
+func checksumPdf(path string) (string, error) {
+	hasher := sha256.New()
+	s, err := ioutil.ReadFile(path)
+	_, err = hasher.Write(s)
+	if err != nil {
+		return "", errors.New("Unable to build checksum")
+	}
+
+	return hex.EncodeToString(hasher.Sum(nil)), nil
+
+}
+func DecryptPdf(inputPath string, outputPath string, encryptionKey []byte) (DecryptedPdf, error) {
+	if _, err := os.Stat(inputPath); err != nil {
+		return DecryptedPdf{}, errors.New("Input file does not exists")
+	}
+
+	// Open Read file
+	input, err := os.Open(inputPath)
+	if err != nil {
+		return DecryptedPdf{}, errors.New("Unable to read input file")
+	}
+
+	// Create output file
+	output, err := os.Create(outputPath)
+	if err != nil {
+		return DecryptedPdf{}, errors.New("Unable to create output file")
+	}
+
+	// decrypt the pdf content, fill the output file
+	encrypter := crypto.NewAESEncrypter_PUBLICATION_RESOURCES()
+	decrypter, ok := encrypter.(crypto.Decrypter)
+	if !ok {
+		return DecryptedPdf{}, errors.New("Unable to create decrypter")
+
+	}
+
+	errDecrypted := decrypter.Decrypt(encryptionKey, input, output)
+
+	if errDecrypted != nil {
+		return DecryptedPdf{}, errors.New("Unable to decrypted file")
+	}
+
+	stats, err := output.Stat()
+	if err != nil || (stats.Size() <= 0) {
+		return DecryptedPdf{}, errors.New("Unable to output file")
+	}
+
+	checksum, err := checksumPdf(outputPath)
+
+	if err != nil {
+		return DecryptedPdf{}, errors.New("Unable to build checksum")
+	}
+
+	output.Close()
+	input.Close()
+	return DecryptedPdf{outputPath, stats.Size(), checksum}, nil
+}
+
+// EncryptPdf Encrypt input file to output file
+func EncryptPdf(inputPath string, outputPath string) (EncryptedPdf, error) {
+	if _, err := os.Stat(inputPath); err != nil {
+		return EncryptedPdf{}, errors.New("Input file does not exists")
+	}
+
+	// Open Read file
+	input, err := os.Open(inputPath)
+	if err != nil {
+		return EncryptedPdf{}, errors.New("Unable to read input file")
+	}
+
+	// Create output file
+	output, err := os.Create(outputPath)
+	if err != nil {
+		return EncryptedPdf{}, errors.New("Unable to create output file")
+	}
+
+	// encrypt the pdf content, fill the output file
+	encrypter := crypto.NewAESEncrypter_PUBLICATION_RESOURCES()
+	encryptionKey, err := encrypter.GenerateKey()
+
+	if err != nil {
+		return EncryptedPdf{}, errors.New("Unable to create encryptionKey")
+	}
+
+	encryptError := encrypter.Encrypt(encryptionKey, input, output)
+
+	if encryptError != nil {
+		return EncryptedPdf{}, errors.New("Unable to encrypt file")
+	}
+
+	stats, err := output.Stat()
+	if err != nil || (stats.Size() <= 0) {
+		return EncryptedPdf{}, errors.New("Unable to output file")
+	}
+
+	checksum, err := checksumPdf(outputPath)
+
+	if err != nil {
+		return EncryptedPdf{}, errors.New("Unable to build checksum")
+	}
+
+	output.Close()
+	input.Close()
+	return EncryptedPdf{outputPath, encryptionKey, stats.Size(), checksum}, nil
+}
---
+++ encrypt/encrypt_test.go
@@ -0,0 +1,96 @@
+package encrypt
+
+import (
+	"os"
+	"testing"
+)
+
+func TestEncryptPdf(t *testing.T) {
+	inputPath, outputPath := "../test/samples/sample.pdf", os.TempDir()+"/encriptedFile.pdf"
+
+	encryptedPdf, error := EncryptPdf(inputPath, outputPath)
+
+	if error != nil {
+		t.Error(error)
+		t.FailNow()
+	}
+
+	if _, err := os.Stat(encryptedPdf.Path); err != nil {
+		t.Error("expected a path")
+
+	}
+
+	if len(encryptedPdf.Checksum) <= 0 {
+		t.Error("expected a Checksum")
+	}
+
+	if encryptedPdf.EncryptionKey == nil {
+		t.Error("expected a key")
+	}
+
+	if len(encryptedPdf.EncryptionKey) <= 0 {
+		t.Error("expected a key is empty")
+		t.Log(encryptedPdf.EncryptionKey)
+	}
+
+	if encryptedPdf.Size <= 0 {
+		t.Error("expected a Size")
+	}
+}
+
+func TestDecryptPdf(t *testing.T) {
+	inputPath, encryptPdfPath := "../test/samples/sample.pdf", os.TempDir()+"/encriptedFile.pdf"
+	decryptInputPdf := os.TempDir() + "/decryptFile.pdf"
+
+	checksumOriginalPDF, error := checksumPdf(inputPath)
+
+	if error != nil {
+		t.Error(error)
+		t.FailNow()
+	}
+
+	encryptedPdf, error := EncryptPdf(inputPath, encryptPdfPath)
+
+	if error != nil {
+		t.Error(error)
+		t.FailNow()
+	}
+
+	if encryptedPdf.EncryptionKey == nil {
+		t.Error("expected a key")
+	}
+
+	if len(encryptedPdf.EncryptionKey) <= 0 {
+		t.Error("expected a key is empty")
+	}
+
+	decryptedPdf, error := DecryptPdf(encryptedPdf.Path, decryptInputPdf, encryptedPdf.EncryptionKey)
+
+	if error != nil {
+		t.Error(error)
+		t.FailNow()
+	}
+
+	if decryptInputPdf != decryptedPdf.Path {
+		t.Error("decrypted Pdf path is not expected")
+	}
+
+	if len(decryptedPdf.Checksum) <= 0 {
+		t.Error("expected a key is empty")
+	}
+
+	if decryptedPdf.Size <= 0 {
+		t.Error("expected a Size")
+	}
+
+	checksumDecrytedPDF, error := checksumPdf(decryptedPdf.Path)
+
+	if error != nil {
+		t.Error(error)
+		t.FailNow()
+	}
+
+	if checksumOriginalPDF != checksumDecrytedPDF {
+		t.Error("The hash for decrypted pdf not math")
+	}
+}
